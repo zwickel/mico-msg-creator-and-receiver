e70134bb12181a4e316b546374781d0ea79741b2
ma <mariusaltmann@outlook.de>
2021-05-10 04:43:12 +0200

    created msg generator it also sends the generated msgs to the web page so it can be displayed

---
+++ src/main/java/io/github/ust/mico/createandreceive/messageprocessing/MessageGenerator.java
@@ -0,0 +1,43 @@
+package io.github.ust.mico.createandreceive.messageprocessing;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+
+import io.github.ust.mico.createandreceive.Sender;
+import io.github.ust.mico.createandreceive.kafka.MicoCloudEventImpl;
+
+public class MessageGenerator {
+
+  @Autowired
+  private CloudEventManipulator cloudEventManipulator;
+
+  @Autowired
+  SimpMessagingTemplate websocketsTemplate;
+
+  @Autowired
+  private Sender sender;
+
+  public MessageGenerator() {
+    try {
+      startProduction();
+    } catch (InterruptedException ie) {
+      // TODO: handle exception
+    }
+  }
+
+  private void startProduction() throws InterruptedException {
+    while (true) {
+      this.produceMessage();
+      Thread.sleep(5000);
+    }
+  }
+
+  private void produceMessage() {
+    MicoCloudEventImpl<JsonNode> cloudEvent = new MicoCloudEventImpl<JsonNode>();
+    cloudEventManipulator.setMissingHeaderFields(cloudEvent, null);
+    websocketsTemplate.convertAndSend("/topic/messaging-bridge", cloudEvent);
+    sender.send(cloudEvent);
+  }
+}
