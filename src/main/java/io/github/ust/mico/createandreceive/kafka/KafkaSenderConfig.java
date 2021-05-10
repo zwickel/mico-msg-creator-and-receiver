84be5b178519e4778f3fdcba9dfa26b95edd29b1
ma <mariusaltmann@outlook.de>
2021-05-10 15:19:05 +0200

    using mico cloud event (de-)serializer

--- src/main/java/io/github/ust/mico/createandreceive/kafka/KafkaSenderConfig.java
+++ src/main/java/io/github/ust/mico/createandreceive/kafka/KafkaSenderConfig.java
@@ -31,7 +31,7 @@ public class KafkaSenderConfig {
     Map<String, Object> props = new HashMap<>();
     props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
     props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, CloudEventSerializer.class);
     return props;
   }
 
